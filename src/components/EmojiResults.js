c7f2ca17d297aca34ecc7c2bed2e63cf00fefdfa
Ishaan-11 <ishaansharma1998@gmail.com>
2021-01-05 16:18:55 +0530

    copy to clipbord on click

--- src/components/EmojiResults.js
+++ src/components/EmojiResults.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import EmojiResultRow from "./EmojiResultRow";
+import Clipboard from "clipboard";
 
 function EmojiResults(props) {
+
+  useEffect(() => {
+    let clipboard = new Clipboard(".copy-to-clipboard");
+    console.log("mount");
+
+    return function cleanup() {
+      console.log("unmount");
+      clipboard.destroy();
+    };
+  });
+
   return (
     <div>
       {props.emojiData.map(emoji => {
